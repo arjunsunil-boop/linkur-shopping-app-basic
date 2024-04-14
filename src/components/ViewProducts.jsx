5826b66a91216f2797408d1620db49631fc46e7d
arjunsunil-boop <arjunsunilgokulan@gmail.com>
2024-04-14 10:49:50 +0530

    Added css to cards to disable overflow text

--- src/components/ViewProducts.jsx
+++ src/components/ViewProducts.jsx
@@ -254,12 +254,11 @@ const ViewProducts = () => {
                 <div className="row">
                     <col className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12" />
 
-                    <div className="row g-3">
+                    <div className="row g-5">
                         {
                             products.map(
                                 (product) => {
-                                return (
-                                    <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
+                                return <div className="col col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 col-xxl-3">
                                         <div className="card" >
                                             <img src={product.image} className="card-img-top product-image" alt="..." />
                                             <div className="card-body">
@@ -271,7 +270,7 @@ const ViewProducts = () => {
                                         </div>
                                     </div>
 
-                                )
+                                
                             
 
 
